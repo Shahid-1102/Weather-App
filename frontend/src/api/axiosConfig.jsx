58ae1fcb3b70fd64e419b7fe51937b1ae1323dfb
SHAHID AHAMED <102601195+Shahid-1102@users.noreply.github.com>
2024-12-24 11:38:01 +0530

    Update axiosConfig.jsx

--- frontend/src/api/axiosConfig.jsx
+++ frontend/src/api/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: `${import.meta.env.VITE_API_URL}`,
+    baseURL: `${import.meta.env.YOUR_API_KEY_HERE}`,
     headers: {
       'Content-Type': 'application/json',
     },
